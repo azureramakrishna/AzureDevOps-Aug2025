81593768f61bb7416a738df6fcb429bc67354e2c
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2025-08-01 10:07:53 +0530

    Update deploy.ps1

--- ARMTemplates/VirtualMachine/deploy.ps1
+++ ARMTemplates/VirtualMachine/deploy.ps1
@@ -1,5 +1,5 @@
 # Login to Azure Portal 
-Login-AzAccount -TenantId "459865f1-a8aa-450a-baec-8b47a9e5c904" -subscriptionId "2e28c82c-17d7-4303-b27a-4141b3d4088f"
+Login-AzAccount -TenantId "your azure tenant id" -subscriptionId "your azure subscription id"
 
 # Create a resource group
 New-AzResourceGroup -Name "MyResourceGroup" -Location "East US"

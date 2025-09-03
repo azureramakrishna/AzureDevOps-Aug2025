1b0f9bf5c8fb77afde1dd32c072ba6f4f682bd5e
Ramakrishna A <aruvaramakrishna@gmail.com>
2025-09-03 09:19:17 +0530

    terraform backend

--- Terraform/MultiResources/backend.tf
+++ Terraform/MultiResources/backend.tf
@@ -2,9 +2,7 @@
 
 terraform {
   backend "azurerm" {
-    use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
-    tenant_id            = "459865f1-a8aa-450a-baec-8b47a9e5c904"  # Can also be set via `ARM_TENANT_ID` environment variable.
-    client_id            = "d7563c32-4271-4b23-a818-a9d0401abaea"  # Can also be set via `ARM_CLIENT_ID` environment variable.
+    resource_group_name = "storage-rg"
     storage_account_name = "saanvikittf"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.

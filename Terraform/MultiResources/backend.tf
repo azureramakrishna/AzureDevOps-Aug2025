aef4886a62c1168481634b22d1fe40443d14de47
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2025-09-03 09:13:14 +0530

    Update backend.tf

--- Terraform/MultiResources/backend.tf
+++ Terraform/MultiResources/backend.tf
@@ -2,7 +2,6 @@
 
 terraform {
   backend "azurerm" {
-    use_oidc             = true                                    # Can also be set via `ARM_USE_OIDC` environment variable.
     use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
     tenant_id            = "459865f1-a8aa-450a-baec-8b47a9e5c904"  # Can also be set via `ARM_TENANT_ID` environment variable.
     client_id            = "d7563c32-4271-4b23-a818-a9d0401abaea"  # Can also be set via `ARM_CLIENT_ID` environment variable.

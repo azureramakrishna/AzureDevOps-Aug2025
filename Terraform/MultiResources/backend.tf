6e859927df964d925d0574854f696f15abf9438e
Ramakrishna A <aruvaramakrishna@gmail.com>
2025-09-03 09:24:48 +0530

    terraform backend

--- Terraform/MultiResources/backend.tf
+++ Terraform/MultiResources/backend.tf
@@ -2,10 +2,10 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name = "storage-rg"
-    storage_account_name = "saanvikittf"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    resource_group_name  = "storage-rg"
+    storage_account_name = "saanvikittf"            # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
